2754e4790fa422b35b4eea7ea4d1a5206dffbf9b
PaymanIB <46563281+PaymanIB@users.noreply.github.com>
2025-06-14 11:48:44 +0100

    Update 1-count.js

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,5 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+//It takes count and add 1 to that. so the count go up by 1 and then again the new count will be taken and will be added by 1. 
